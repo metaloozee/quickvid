514a10cda84151af9faf8476a990588de17d5766
Ayan <ayanparkar123@gmail.com>
2024-05-03 22:29:16 +0530

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,6 @@ export const metadata: Metadata = {
     description: siteConfig.description,
     icons: {
         icon: "/favicon.ico",
-        shortcut: "/favicon-16x16.png",
-        apple: "/apple-touch-icon.png",
     },
 }
 
