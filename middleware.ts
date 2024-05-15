91618935225f3819e15eba6570c2c58ff87b839d
Ayan <ayanparkar123@gmail.com>
2024-05-15 13:21:48 +0530

    fix: redirects

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,16 @@ import { NextResponse, type NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
     // Comment the below code if you are hosting it on your server.
-    if (
-        request.nextUrl.hostname !== "quickvid.oozee.me" &&
-        request.nextUrl.hostname !== "localhost"
-    ) {
-        console.log("Redirecting user to quickvid.oozee.me ...")
+    // if (
+    //     request.nextUrl.hostname !== "quickvid.oozee.me" &&
+    //     request.nextUrl.hostname !== "localhost"
+    // ) {
+    //     console.log("Redirecting user to quickvid.oozee.me ...")
 
-        return NextResponse.redirect(
-            new URL("https://quickvid.oozee.me", request.url)
-        )
-    }
+    //     return NextResponse.redirect(
+    //         new URL("https://quickvid.oozee.me", request.url)
+    //     )
+    // }
 
     return NextResponse.next()
 }
