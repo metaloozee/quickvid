10d9226bae17dc1dbe6ab99f0262979541a0715c
Ayan <ayanparkar123@gmail.com>
2024-06-04 20:53:35 +0530

    fix: safety settings

--- lib/core/summarize.ts
+++ lib/core/summarize.ts
@@ -1,4 +1,5 @@
 import { env } from "@/env.mjs"
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai"
 import { ChatPromptTemplate } from "@langchain/core/prompts"
 import { RunnableMap } from "@langchain/core/runnables"
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
@@ -20,6 +21,28 @@ export const summarizeTranscriptWithGemini = async (
         model: model,
         temperature: 0,
         streaming: false,
+        safetySettings: [
+            {
+                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+            },
+        ],
     })
 
     try {
