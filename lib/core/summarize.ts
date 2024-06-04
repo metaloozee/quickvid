e94ec3e35faff2b184698b3e850cf86dac0fbe30
Ayan <ayanparkar123@gmail.com>
2024-06-04 22:23:32 +0530

    fix: category bug

--- lib/core/summarize.ts
+++ lib/core/summarize.ts
@@ -38,10 +38,6 @@ export const summarizeTranscriptWithGemini = async (
                 category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
                 threshold: HarmBlockThreshold.BLOCK_NONE,
             },
-            {
-                category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-            },
         ],
     })
 
