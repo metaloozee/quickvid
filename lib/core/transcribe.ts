9f303567be169c2051df60ed9d0bb143527b367d
Ayan <ayanparkar123@gmail.com>
2024-08-30 12:28:21 +0530

    fix: youtubei.js for transcript

--- lib/core/transcribe.ts
+++ lib/core/transcribe.ts
@@ -1,31 +1,40 @@
-import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube"
+import { Innertube } from "youtubei.js/web"
 
 import { uploadAndTranscribe } from "@/lib/core/convert"
 
 export const transcribeVideo = async (link: string) => {
-    const loader = YoutubeLoader.createFromUrl(link, { addVideoInfo: true })
+    const youtube = await Innertube.create({
+        location: "US",
+        lang: "en",
+        retrieve_player: false,
+    })
 
-    try {
-        const docs = await loader.load()
-        if (!docs[0].pageContent) {
-            throw new Error("An error occurred while transcribing the audio.")
-        }
+    // try {
+    const videoId = new URL(link).searchParams.get("v") as string
+    const info = await youtube.getInfo(videoId)
+    const transcriptData = await info.getTranscript()
 
-        return docs[0].pageContent
-    } catch (e) {
-        try {
-            console.log("Couldnt find captions... running whsiper...")
-            const transcript = await uploadAndTranscribe(link)
-            if (!transcript) {
-                throw new Error(
-                    "An error occurred in whisper while transcribing the audio."
-                )
-            }
-
-            return transcript
-        } catch (e) {
-            console.error(e)
-            return null
-        }
+    if (!transcriptData) {
+        throw new Error("No transcript data found")
     }
+
+    return transcriptData?.transcript?.content?.body?.initial_segments
+        .map((segment) => segment.snippet.text)
+        .join(" ")
+    // } catch (e) {
+    //     try {
+    //         console.log("Couldnt find captions... running whsiper...")
+    //         const transcript = await uploadAndTranscribe(link)
+    //         if (!transcript) {
+    //             throw new Error(
+    //                 "An error occurred in whisper while transcribing the audio."
+    //             )
+    //         }
+
+    //         return transcript
+    //     } catch (e) {
+    //         console.error(e)
+    //         return null
+    //     }
+    // }
 }
